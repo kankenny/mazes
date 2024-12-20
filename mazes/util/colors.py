ca45aa913f4770fff823e27154013e7bd30a84df
kenny <97374837+kankenny@users.noreply.github.com>
2024-12-20 19:55:18 +0000

    shame: pathetic attempt of coloring

---
+++ mazes/util/colors.py
@@ -0,0 +1,16 @@
+# def color(color: str, dark: float, bright: float) -> tuple:
+#     match color:
+#         case "reds":
+#             return 255, bright, bright
+#         case "greens":
+#             return dark, dark, bright
+#         case "blues":
+#             return bright, bright, dark
+#         case "purples":
+#             return bright, dark, bright
+#         case "teals":
+#             return dark, bright, bright
+#         case "grays":
+#             return dark, dark, dark
+#         case _:
+#             return dark, dark, bright
