e87f7438e5bd51539bb2b62382cd17de044b310f
kenny <97374837+kankenny@users.noreply.github.com>
2025-01-10 00:13:50 +0000

    test: add colored maze variations

---
+++ mazes/mazes/__init__.py
@@ -0,0 +1,5 @@
+from .binary_tree import BinaryTree
+from .aldous_broder import AldousBroder
+from .sidewinder import Sidewinder
+
+__all__ = ['BinaryTree', 'AldousBroder', 'Sidewinder']
--- tests/integration/test_colored_grid.py
+++ tests/integration/test_colored_grid.py
@@ -1,18 +1,21 @@
+import random
+
 from mazes.grids.colored_grid import ColoredGrid
-from mazes.mazes.binary_tree import BinaryTree
+from mazes.mazes import AldousBroder, BinaryTree, Sidewinder
 
 
 def test_colored_grid():
-    grid = ColoredGrid(25, 25)
-    maze = BinaryTree(grid)()
-
-    start_coord = maze.rows // 2, maze.cols // 2
-    start = maze[start_coord]
-
-    distances = start.distances()
-    maze.distances = distances
-
     colors = ["reds", "blues", "yellows", "greens", "purples", "oranges", "blacks"]
 
     for color in colors:
+        grid = ColoredGrid(25, 25)
+    
+        maze_generator = random.choice([AldousBroder, BinaryTree, Sidewinder])
+        maze = maze_generator(grid)()
+
+        start_coord = maze.rows // 2, maze.cols // 2
+        start = maze[start_coord]
+
+        distances = start.distances()
+        maze.distances = distances
         maze.to_png(output_name=f"examples/colored_maze_{color}.png", cell_color=color)
