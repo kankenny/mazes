7be6f183f5fcba83af3a261ababf7690151c4bf7
kenny <97374837+kankenny@users.noreply.github.com>
2025-01-16 22:37:09 +0000

    chore: remove unneeded op

--- mazes/util/vid_optimizer.py
+++ mazes/util/vid_optimizer.py
@@ -1,5 +1,5 @@
 from moviepy import VideoFileClip  # type: ignore
-from moviepy.video.fx import AccelDecel, Resize  # type: ignore
+from moviepy.video.fx import AccelDecel  # type: ignore
 
 
 def optimize_gif(
@@ -10,6 +10,5 @@ def optimize_gif(
     fps: int = 30,
 ) -> None:
     clip = VideoFileClip(file_name)
-    clip = Resize(dimension).apply(clip)
     clip = AccelDecel(new_duration=duration).apply(clip)
     clip.write_gif(file_name, fps=30, loop=loop)
